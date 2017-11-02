931afb1754e5acfafca67ff803df3385067095cb
王帆 <wangfan@wFan-2.local>
2017-11-02 17:59:04 +0800

    create schoolpal-framework project !

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./dist/css"));
+});
